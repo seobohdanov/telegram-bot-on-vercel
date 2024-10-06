6f21084bdb6746f1e623371de0356436214fe31f
seobohdanov <seobohdanov@gmail.com>
2024-10-06 13:28:33 +0200

    Изменения для использования Webhook

--- bot.js
+++ bot.js
@@ -1,9 +1,11 @@
 const { Telegraf } = require('telegraf');
 const axios = require('axios');
+const express = require('express');
 require('dotenv').config();
 
-// Ваш токен Телеграм-бота
+// Инициализация бота с токеном
 const bot = new Telegraf(process.env.TELEGRAM_TOKEN);
+const app = express();
 
 // URL вашего Webhook в Make.com
 const webhookUrl = 'https://hook.eu2.make.com/6jg7iy33vtkfeqb1yi4sdpu2tugped3x';
@@ -76,9 +78,15 @@ bot.on('text', async (ctx) => {
   userSessions[chatId] = session;
 });
 
-// Запуск бота
-bot.launch();
+// Настройка Webhook и экспресс-сервера
+const webhookPath = `/bot${process.env.TELEGRAM_TOKEN}`;
+bot.telegram.setWebhook(`${process.env.VERCEL_URL}${webhookPath}`);
+app.use(bot.webhookCallback(webhookPath));
 
-// Graceful stop
-process.once('SIGINT', () => bot.stop('SIGINT'));
-process.once('SIGTERM', () => bot.stop('SIGTERM'));
+// Простой ответ для проверки
+app.get('/', (req, res) => {
+  res.send('Бот успешно работает через Webhook!');
+});
+
+// Экспорт приложения для использования на Vercel
+module.exports = app;
